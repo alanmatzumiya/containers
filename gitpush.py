63f21a844394e8eb7075ff468d290e84fcd85a03
alanmatzumiya <alan.matzumiya@outlook.es>
2021-02-03 19:40:43 -0700

    auto

--- gitpush.py
+++ gitpush.py
@@ -1,2 +1,2 @@
 import os
-os.system("git init && git add . && git commit -m 'auto' && git push origin music_7")
+input(os.system("git init && git add . && git commit -m 'auto' && git push origin music_7"))
