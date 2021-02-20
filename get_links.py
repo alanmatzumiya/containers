09fe73036e9d229f5d9145281fe3f020909bb5de
Alan Matzumiya <alan.matzumiya@outlook.es>
2021-02-20 16:17:02 -0700

    Update get_links.py

--- get_links.py
+++ get_links.py
@@ -7,3 +7,12 @@ list_tags = soup.find_all(class_="js-navigation-open link-gray-dark")
 element = list_tags[0]
 link = element.get("href")
 print(link)
+
+url = "https://github.com/alanmatzumiya/containers/"
+branches, first = [], []
+for j in range(1, 11):
+    branches.append("music_" + str(j))
+    dirs.append("part_" + str(j))
+
+
+
